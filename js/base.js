a6565f2d6be1bafaad01d940c90c2910b9995506
Eric Huffman <eric@erichuffman.com>
2015-05-04 19:36:19 -0400

    cleaning up js file and comments in positioning partial

--- js/base.js
+++ js/base.js
@@ -2,7 +2,7 @@
  * @file
  * js base functions
  *
- * Includes js behaviors for various toggles and switches
+ * Includes js behaviors for menus and other behaviors 
  *
  */
 
@@ -10,11 +10,16 @@
 Drupal.behaviors.eh_dot_com = {
   attach: function (context, settings) {
       
+      /**
+       * put a fixed height on footer in desktop viewports
+       *
+       */
       // wait until all sub-elements are fully loaded
       $(window).load(function() {
         // get height of viewport
         var vph = $(window).height();
-        console.log(vph);
+        // debugging
+        // console.log(vph);
         // check that viewport is larger than 1024px
         if ($(window).width() > '1024') {
           // Set fixed height on footer
@@ -22,54 +27,62 @@ Drupal.behaviors.eh_dot_com = {
         }
       });
       
+      /**
+       * menu behavior based on scroll action
+       *
+       */
       var about = $('#about').offset().top - 100;
       var work = $('#work').offset().top - 100;
       var contact = $('#contact').offset().top - 100;
       $(window).scroll(function () {
-        // what is the y position of the scroll?
+        // get scroll position
         var y = $(window).scrollTop();    
-        // whether that's below the start of article?
+        // deactivate all menu items if above 'about' section
         if (y < about) {
           $(".primary-menu-wrapper .menu li").each(function() {
             $(this).removeClass('active');
           });
         }
+        // activate about link item if within section
         if (y >= about) {
           $(".primary-menu-wrapper .menu li").each(function() {
             $(this).removeClass('active');
           });
           $('#about-link').addClass('active');
         } 
+        // activate work link item if within section
         if (y >= work) {
           $(".primary-menu-wrapper .menu li").each(function() {
             $(this).removeClass('active');
           });
           $('#work-link').addClass('active');
         } 
+        // activate contact link item if within section
         if (y >= contact) {
           $(".primary-menu-wrapper .menu li").each(function() {
             $(this).removeClass('active');
           });
           $('#contact-link').addClass('active');
         }
-        console.log(y);
+        // debugging
+        // console.log(y);
       });
       
-      
+      // mobile menu close action on menu item click
       $(".primary-menu-wrapper .menu li a").click(function(event) {
         if ($(this).parent('li').parent('ul').hasClass('opened')) {
           $(this).parent('li').parent('ul').removeClass('opened');
         }
       }); 
       
-      // using menu title for mobile view toggle
+      // menu toggle for mobile menu
       $(".block-title-wrapper h4").click(function(event) {
         $(".primary-menu-wrapper .menu").toggleClass('opened');
       });
       
       // "Smooth Scroll" function
-      // add smooth-scroll class to an anchor link and watch the magic happen
-      $(".smooth-scroll").click(function(event){		
+      // adds smooth-scroll action to an anchor links with smooth-scroll class
+      $("a.smooth-scroll").click(function(event){		
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top - 60}, 500);
       });
