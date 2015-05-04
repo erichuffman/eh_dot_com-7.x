0b0da0f2265d84419a814a3527e6469687d5e8f7
Eric Huffman <eric@erichuffman.com>
2015-05-04 16:41:06 -0400

    Adding breakpoint and bunlder support

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Pull gems from RubyGems
+source "https://rubygems.org"
+
+gem 'sass', '~>3.4.13'
+gem 'compass', '~>1.0.3'
+gem 'breakpoint', '~>2.0'
--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'breakpoint'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -16,9 +17,3 @@ javascripts_dir = "js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
